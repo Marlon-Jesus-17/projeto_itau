9cea59674ca93a67fd2aecc348d2da814ea45c80
Marlon <maarlonsantos17@gmail.com>
2026-06-22 20:12:37 -0300

    feat: Adiciona as anotations para gets/setters e contructs

--- ItauJava/src/main/java/com/marlebas/ItauJava/Transacao.java
+++ ItauJava/src/main/java/com/marlebas/ItauJava/Transacao.java
@@ -1,8 +1,15 @@
 package com.marlebas.ItauJava;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transacao {
 
     private BigDecimal valor; //BigDecimal - usado para valores com precisão absoluta, como dinheiro, por exemplo
