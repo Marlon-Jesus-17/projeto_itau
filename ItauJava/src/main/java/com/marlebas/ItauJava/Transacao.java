0cf18e2173baf351d4ad01e2194fb78dfde196a3
Marlon <maarlonsantos17@gmail.com>
2026-06-22 19:34:09 -0300

    feat: Cria as classes Transação e TransaçãoController

--- ItauJava/src/main/java/com/marlebas/ItauJava/Transacao.java
+++ ItauJava/src/main/java/com/marlebas/ItauJava/Transacao.java
@@ -1,6 +1,10 @@
 package com.marlebas.ItauJava;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
 public class Transacao {
 
-    
+    private BigDecimal valor; //BigDecimal - usado para valores com precisão absoluta, como dinheiro, por exemplo
+    private OffsetDateTime dataHora; //OffsetDateTime - Armazena data e hora completas incluíndo o fuso horário
 }
---
+++ ItauJava/src/main/java/com/marlebas/ItauJava/TransacoesController.java
@@ -0,0 +1,5 @@
+package com.marlebas.ItauJava;
+
+public class TransacoesController {
+
+}
